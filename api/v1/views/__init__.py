e006e6a6a268327a344b5929461faff395fa9968
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-17 14:09:17 +0000

    added

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ init """
 from flask import Blueprint
+from models.engine.db_storage import DBStorage
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
+
+storage = DBStorage
+storage.reload()
